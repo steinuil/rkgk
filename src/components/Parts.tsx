1fd6682f00c3581d569477cbdf89862e9c37bfc2
steenuil <steenuil.owl@gmail.com>
2017-08-20 17:25:37 +0200

    notification test

---
+++ src/components/Parts.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+
+export const Loader = (props: { floating: boolean }) =>
+  <div className={"loader" + (props.floating ? "floating" : "")}>
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+    <div className="box" />
+  </div>;
+
+
+export const LoadPage = (props: { text: string }) =>
+  <div id="loader-root">
+    <Loader floating={false} />
+    <p>{props.text}</p>
+  </div>;
+
+
+export const Heart =
+  (props: { classes: string, color: string, click: (ev: any) => any }) =>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60"
+      className={"button " + props.classes} onClick={props.click} >
+      <path fill={props.color} stroke="#45425a" className="heart"
+        d="M 30,52  l -20,-20  c -10,-10 0,-30 20,-20  c 20,-10 30,10 20,20 Z" />
+    </svg>;
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -2,8 +2,8 @@
 // Manages the login tokens and outputs
 // the main page or the login page accordingly.
 import * as React from "react";
-import * as Login from "./Login";
 import * as Pixiv from "../pixiv";
+import * as Login from "./Login";
 import { LoadPage } from "./Parts";
 import { Browser } from "./Browser";
 
@@ -15,9 +15,27 @@ export type Status<T> = null | undefined | T;
 const maybe = undefined;
 
 
+export interface Notif {
+  id: number;
+  message: string;
+}
+
+
+const Toast = (props: { notif: Notif, click: (id: number) => any }) =>
+  <div className="notification button" onClick={_ => props.click(props.notif.id)}>
+    {props.notif.message}
+  </div>;
+
+
+const mkNotif = (message: string) => {
+  return { id: (new Date()).getTime(), message: message };
+}
+
+
 export interface State {
   api: Status<Pixiv.API>;
   account: null | Pixiv.MyInfo;
+  notifs: Array<Notif>;
 }
 
 
@@ -27,9 +45,10 @@ export class App extends React.Component<{}, State> {
 
     // Attempt to retrieve a previously stored refresh token from localStorage,
     // otherwise set the state to null.
+    /*
     const token = window.localStorage.getItem('rkgk_token');
     if (token) {
-      this.state = { api: maybe, account: null };
+      this.state = { api: maybe, account: null, notifs: [] };
       (new Pixiv.API()).init(token)
         .then(([api, info]) =>
           this.setState({ api: api, account: info }))
@@ -39,8 +58,11 @@ export class App extends React.Component<{}, State> {
           window.localStorage.removeItem('rkgk_token');
         });
     } else {
-      this.state = { api: null, account: null };
+    */
+      this.state = { api: null, account: null, notifs: [] };
+    /*
     }
+    */
   }
 
   handleLogin(args: [Pixiv.Tokens, Pixiv.MyInfo]) {
@@ -48,6 +70,21 @@ export class App extends React.Component<{}, State> {
     window.localStorage.setItem('rkgk_token', tokens.refresh);
     (new Pixiv.API()).init(tokens.refresh, tokens.access, tokens.expires)
       .then(([api, _]) => this.setState({ api: api, account: info }));
+  }
+
+  notify(message: string) {
+    const num = this.state.notifs.length;
+    this.setState({
+      notifs: this.state.notifs.concat([
+        mkNotif(message.repeat(num + 1))
+      ])
+    });
+  }
+
+  dismissNotif(id: number) {
+    this.setState({
+      notifs: this.state.notifs.filter(e => e.id != id)
+    });
   }
 
   render() {
@@ -59,6 +96,14 @@ export class App extends React.Component<{}, State> {
     : // api is available
         <Browser api={this.state.api} />
 
-    return main;
+    return <div id="page-root">
+      <button id="test" onClick={_ => this.notify("test")}>test</button>
+      <div id="notif-box">
+        {this.state.notifs.map(n =>
+          <Toast key={n.id} click={id => this.dismissNotif(id)} notif={n} />
+        )}
+      </div>
+      {main}
+    </div>;
   }
 }
