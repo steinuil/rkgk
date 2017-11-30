2a08de2aa14fc1deb7bb349c0ba1d8465c2d46c5
steinuil <steenuil.owl@gmail.com>
2017-11-30 12:30:34 +0100

    readded and integrated notifications

--- src/gui/App.tsx
+++ src/gui/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as Pixiv from "../pixiv/api";
+import * as Notify from "./Notify";
 import LoginForm from "./Login";
+
 
 
 export interface Props {
@@ -9,14 +11,60 @@ export interface Props {
 
 
 export interface State {
+  notifs: Notify.State;
 }
 
 
 export class App extends React.Component<Props, State> {
+  state = {
+    notifs: { list: [], counter: 0 }
+  };
+
+
+  componentDidMount() {
+    // try to log in with the refresh token
+  }
+
+
+  private addNotif = (msg?: string) => {
+    if (!msg) return;
+
+    this.setState((prev, _) => {
+      const notifs = prev.notifs;
+      const notif = {
+        id: notifs.counter,
+        message: msg
+      };
+
+      const newNotifs = {
+        list: notifs.list.concat([notif]),
+        counter: notifs.counter + 1
+      };
+
+      return {
+        notifs: newNotifs
+      };
+    });
+  };
+
+
+  private dismissNotif = (id: number) => {
+    this.setState((prev, _) => {
+      return {
+        notifs: {
+          list: prev.notifs.list.filter(n => n.id != id),
+          counter: prev.notifs.counter
+        }
+      };
+    });
+  };
+
+
   render() {
     return <div>
         <LoginForm initialUsername={undefined} onSubmit={(u, p) => console.log(u)}
-          notify={(msg) => console.log(msg)} />
+          notify={(msg) => this.addNotif(msg)} />
+        <Notify.Box notifs={this.state.notifs.list} onDismiss={id => this.dismissNotif(id)} />
       </div>;
   }
 }
---
+++ src/gui/Notify.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+
+export interface Notif {
+  id: number;
+  message: string;
+}
+
+
+interface ToastProps {
+  notif: Notif;
+  onClick: (id: number) => void;
+}
+
+
+const Toast = (props: ToastProps) =>
+  <div className="notification">
+    <p>{props.notif.message}</p>
+    <svg xmlns="http://www.w3.org/2000/svg"
+        className="button cross" viewBox="0 0 50 50"
+        onClick={_ => props.onClick(props.notif.id)} >
+      <line x1="5" y1="5" x2="45" y2="45" />
+      <line x1="45" y="5" x2="5" y2="45" />
+    </svg>
+  </div>;
+
+
+export interface State {
+  list: Array<Notif>;
+  counter: number;
+}
+
+
+export interface BoxProps {
+  notifs: Array<Notif>;
+  onDismiss: (id: number) => void;
+}
+
+
+export const Box = (props: BoxProps) => {
+  const notifs = props.notifs.map((n) =>
+    <Toast key={n.id} notif={n} onClick={props.onDismiss} />
+  );
+
+  return <div id="notif-box">{notifs}</div>;
+};
